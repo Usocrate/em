de545b2f9eaf1ea0e65fc8daf3cd24633282da9d
Florent <Florent@Traktor>
2016-03-14 23:07:40 +0100

    nodejs : Récupération d'argument passé en ligne de commande

--- node/mysql-test.js
+++ node/mysql-test.js
@@ -12,13 +12,21 @@ fs.readFile('../config/host.json', (err, data) => {
 	    "password" : config.db_password,
 	    "database" : config.db_name
 	  });
-	   
+	  
 	  connection.connect();
-
-	  connection.query('SELECT COUNT(*) AS nb FROM bookmark WHERE bookmark_thumbnail_filename IS NULL', function(err, rows, fields) {
-		  if (err) throw err;
-		  console.log('Sans aperçu : ', rows[0].nb, 'signet(s)');
-	  });
-	   
+	  
+	  if (process.argv.length == 3) {
+		  console.log('Identifiant signet : ', process.argv[2]);
+		  var id = process.argv[2];
+		  var query = connection.query('SELECT bookmark_title AS title, bookmark_url AS url,bookmark_thumbnail_filename AS filename FROM bookmark WHERE bookmark_id=?',id);
+		  query.on('result', function(row){
+			  console.log(row.url,'(',row.title,') : ',row.filename);
+		  });
+	  } else {
+		  connection.query('SELECT COUNT(*) AS nb FROM bookmark WHERE bookmark_thumbnail_filename IS NULL', function(err, rows, fields) {
+			  if (err) throw err;
+			  console.log('Sans aperçu : ', rows[0].nb, 'signet(s)');
+		  });
+	  }
 	  connection.end();
 });
