0e7c93e1bd0b8ead55f8d60b438225cc5be8a5bb
Florent <Florent@Chosta>
2016-03-27 13:50:19 +0200

    node : modularisation de la config; imagemagick ajouté au package

---
+++ node/config.js
@@ -0,0 +1,7 @@
+var fs = require('fs');
+
+fs.readFile('../config/host.json', (err, file_content) => {
+	  if (err) throw err;	  
+	  var data = JSON.parse(file_content);
+	  exports.getMySqlConnectionParams = () => JSON.parse('{"host":"'+data.db_host+'","database":"'+data.db_name+'","user":"'+data.db_user+'","password":"'+data.db_password+'"}');
+});
--- node/server.js
+++ node/server.js
@@ -1,8 +1,10 @@
 var http = require('http');
+const config = require('./config.js');
 
 http.createServer(function(request, response){
 	response.writeHead(200);
-	response.write('Server actif !');
+	var mySqlConnectionParams = config.getMySqlConnectionParams();
+	response.write('Bienvenue '+ mySqlConnectionParams.user+' !');
 	response.end();
 }
 ).listen(8080);
