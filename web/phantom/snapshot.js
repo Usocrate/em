12e27549dfe506566ba51ad7d71b9e1a798edfb2
Florent <Florent@Traktor>
2016-07-15 12:55:22 +0200

    Ajout timeout sur génération de snapshot

--- web/phantom/snapshot.js
+++ web/phantom/snapshot.js
@@ -4,9 +4,10 @@
 var system = require('system');
 var page = require('webpage').create();
 
-console.log('capture attendue ',system.args[1]);
+console.log('capture attendue : ',system.args[1]);
 console.log('fichier de destination : ',system.args[2]);
 
+page.settings.resourceTimeout = 7000; // 7 seconds
 page.viewportSize = {
 	width : 1024,
 	height : 768
@@ -17,7 +18,13 @@ page.clipRect = {
 	width : 1024,
 	height : 768
 };
+page.onResourceTimeout = function(e) {
+  console.log(e.errorCode);   // it'll probably be 408 
+  console.log(e.errorString); // it'll probably be 'Network timeout on resource'
+  console.log(e.url);         // the url whose request timed out
+  phantom.exit(1);
+};
 page.open(system.args[1], function() {
-	page.render(system.args[2]);
+	page.render(system.args[2],{format: 'jpeg', quality: '100'});
 	phantom.exit();
 });
