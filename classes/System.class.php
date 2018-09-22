3c62d32251c0a7710f2bf3c65bf8ada454a54ba4
Florent Chanavat <florent@usocrate.fr>
2018-09-22 15:40:31 +0000

    Correction emplacement manifest

--- classes/System.class.php
+++ classes/System.class.php
@@ -434,7 +434,7 @@ class System
 		$output = array();
 		$output[] = '<link rel="icon" type="image/png" sizes="32x32" href="'.$this->getSkinUrl().'/images/favicon-32x32.png">';
 		$output[] = '<link rel="icon" type="image/png" sizes="16x16" href="'.$this->getSkinUrl().'/images/favicon-16x16.png">';
-		$output[] = '<link rel="manifest" href="'.$this->getSkinUrl().'/site.webmanifest">';
+		$output[] = '<link rel="manifest" href="'.$this->getSkinUrl().'/manifest.json">';
 		$output[] = '<meta name="application-name" content="'.ToolBox::toHtml( $this->getProjectName() ).'">';
 		$output[] = '<meta name="theme-color" content="#8ea4bc">';
 		return $output;
